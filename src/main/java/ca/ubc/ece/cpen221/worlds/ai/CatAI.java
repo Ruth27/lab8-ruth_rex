0fd5b2ca6bce5b99a49f9ea7c2ca8c9f584339ed
Ruth_Y <ruthyucanada@gmail.com>
2022-11-21 17:12:01 -0800

    Added Animal Subtype (cat) and AIs.

--- src/main/java/ca/ubc/ece/cpen221/worlds/ai/CatAI.java
+++ src/main/java/ca/ubc/ece/cpen221/worlds/ai/CatAI.java
@@ -1,5 +1,43 @@
 package ca.ubc.ece.cpen221.worlds.ai;
 
-public class CatAI extends AbstractAI {
+import ca.ubc.ece.cpen221.worlds.ArenaWorld;
+import ca.ubc.ece.cpen221.worlds.Direction;
+import ca.ubc.ece.cpen221.worlds.Location;
+import ca.ubc.ece.cpen221.worlds.Util;
+import ca.ubc.ece.cpen221.worlds.commands.Command;
+import ca.ubc.ece.cpen221.worlds.commands.EatCommand;
+import ca.ubc.ece.cpen221.worlds.commands.MoveCommand;
+import ca.ubc.ece.cpen221.worlds.commands.WaitCommand;
+import ca.ubc.ece.cpen221.worlds.items.Item;
+import ca.ubc.ece.cpen221.worlds.items.animals.ArenaAnimal;
 
+import java.util.Set;
+
+public class CatAI extends AbstractAI {
+    private final int closest = 5; // max number; greater than cat's view range
+    private int energy;
+
+    public CatAI() {
+
+    }
+
+    @Override
+    public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
+        Direction dir = Util.getRandomDirection();
+        Location targetLocation = new Location(animal.getLocation(), dir);
+        Set<Item> possibleEats = world.searchSurroundings(animal);
+        Location current = animal.getLocation();
+        for (Item item : possibleEats) {
+            if ((item.getName().equals("Gnat") || item.getName().equals("grass"))
+                && (current.getDistance(item.getLocation()) == 1)) {
+                return new EatCommand(animal, item); // arena animals eat cats
+                // and rabbits
+            }
+        }
+        if (Util.isValidLocation(world, targetLocation)
+            && this.isLocationEmpty(world, animal, targetLocation)) {
+            return new MoveCommand(animal, targetLocation);
+        }
+        return new WaitCommand();
+    }
 }
